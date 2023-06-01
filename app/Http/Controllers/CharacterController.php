023dccff018affe62426bbd54a5f2f559997dff6
Antonio Anerão <anerao.junior@gmail.com>
2023-05-31 20:26:52 -0500

    Added: Character edit page and table style to Character index

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Character;
 use Illuminate\View\View;
 
 class CharacterController extends Controller
 {
+    /**
+     * @property Character $character
+     */
     public function __construct()
     {
         $this->middleware('auth');
@@ -14,5 +18,9 @@ class CharacterController extends Controller
     public function index(): View
     {
         return view('character/index');
+    }
+
+    public function edit(Character $character) {
+        return view('character/edit', compact('character'));
     }
 }
--- resources/views/character/index.blade.php
+++ resources/views/character/index.blade.php
@@ -1,23 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    <ul>
-                        @forelse(auth()->user()->characters as $character)
-                            <li>{{ $character->Name }}</li>
-                        @empty
-                            Ops
-                        @endforelse
-                    </ul>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">Character List</div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Resets</th>
+                                    <th scope="col">Points</th>
+                                    <th scope="col">#</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse(auth()->user()->characters as $character)
+                                    <tr>
+                                        <td>{{ $character->Name }}</td>
+                                        <td>{{ $character->Resets }}</td>
+                                        <td>{{ $character->LevelUpPoints }}</td>
+                                        <td><a href="{{ route('character.edit', $character->Id) }}">View</a>
+                                    </tr>
+                                @empty
+                                    <div class="alert alert-info">
+                                        You don't have any character yet!
+                                    </div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
