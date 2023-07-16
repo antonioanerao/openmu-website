b083057d71662e4cba913451ad588884e1092a92
Antonio Anerão <anerao.junior@gmail.com>
2023-07-16 12:43:41 -0500

    Added recovery password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 
 class ResetPasswordController extends Controller
 {
@@ -22,9 +25,63 @@ class ResetPasswordController extends Controller
     use ResetsPasswords;
 
     /**
+     * Changes the default password column name from
+     * password to PasswordHash
+     */
+    protected function setUserPassword($user, $password)
+    {
+        $user->PasswordHash = Hash::make($password);
+    }
+
+    /**
+     * Cutom class to Reset the given user's password.
+     *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
+     * @return void
+     */
+    protected function resetPassword($user, $password)
+    {
+        $this->setUserPassword($user, $password);
+        $user->save();
+        event(new PasswordReset($user));
+        $this->guard()->login($user);
+    }
+    /**
      * Where to redirect users after resetting their password.
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function reset(Request $request)
+    {
+        $request->validate($this->rules(), $this->validationErrorMessages());
+
+        $response = $this->broker()->reset(
+            $this->credentials($request), function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+        );
+
+        return $response == Password::PASSWORD_RESET ?
+            $this->sendResetResponse($request, $response) :
+            $this->sendResetFailedResponse($request, $response);
+    }
+
+
+    protected function getEmailCredentials(Request $request)
+    {
+        return [
+            'EMail' => $request->input('email'),
+            'password' => $request->input('password'),
+            'password_confirmation' => $request->input('password_confirmation'),
+            'token' => $request->input('token'),
+        ];
+    }
+
+    protected function credentials(Request $request)
+    {
+        return $this->getEmailCredentials($request);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -44,6 +45,38 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime', 'password' => 'hashed', 'Id' => 'string',
         'VaultId' => 'string'
     ];
+
+    /**
+     * Changes the defaul email column name to the
+     * reset password method
+     *
+     * @return string
+     */
+    public function getEmailForPasswordReset(): string
+    {
+        return $this->EMail;
+    }
+
+    /**
+     * Override the routeNotificationFor method to use
+     * the custom email column name. It is needed to
+     * send the e-mail reset password link
+     */
+    public function routeNotificationFor($driver)
+    {
+        if (method_exists($this, $method = 'routeNotificationFor'.Str::studly($driver))) {
+            return $this->{$method}();
+        }
+
+        switch ($driver) {
+            case 'database':
+                return $this->notifications();
+            case 'mail':
+                return $this->EMail;
+            case 'nexmo':
+                return $this->phone_number;
+        }
+    }
 
     /**
      * Return a list of all characters from this account
